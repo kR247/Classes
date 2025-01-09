e9e4df68295ec53e9f5cd116065eb8967fa87fc4
kR247 <kaluginroman7@icloud.com>
2025-01-10 00:08:00 +0300

    added special method __radd__

--- classes_2nd_lesson.py
+++ classes_2nd_lesson.py
@@ -19,27 +19,47 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors < other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors > other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors <= other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
-        self.number_of_floors += value
-        return self.number_of_floors
+        if isinstance(value, int):
+            x = self.number_of_floors
+            y = value
+            z = x + y
+            return z
 
+    def __radd__(self, value):
+        y = self.number_of_floors
+        x = value
+        z = x.__add__(y)
+        return z
 
 
 
@@ -52,8 +72,15 @@ h2.go_to(32)
 
 print(h1)
 print(h2)
-
 print(len(h1))
 print(len(h2))
 
-print(h1+10)
+print(h1 + 10)  # __add__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
+print(11 + h1)  # __radd__
+
+
